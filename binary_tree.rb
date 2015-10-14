7fe9f4fe5144a65fb1bbda825f9ca170cd9e6a5c
malty-ale <eric.maltiel@gmail.com>
2015-10-14 01:21:06 +0000

    add search functions

--- binary_tree.rb
+++ binary_tree.rb
@@ -7,14 +7,15 @@ class Node
 end
 
 class Tree
+  
   def initialize(root)
     @root = Node.new(root)
   end
 
   def build_tree(array)
-    tree = [@root]
+    @tree = [@root]
     array.each do |i|
-      tree << new_node(i, @root)
+      @tree << new_node(i, @root)
     end
   end
   
@@ -39,7 +40,45 @@ class Tree
       end
     end
   end
+  
+  def breadth_first_search(target)
+    queue = [@root]
+    
+    while !queue.empty?
+      test = queue.shift
+      
+      return target if test.value == target
+      
+      queue << test.left_child if !test.left_child.nil?
+      queue << test.right_child if !test.right_child.nil?
+    end
+    
+    return nil
+  end
+  
+  def depth_first_search(target)
+    stack = [@root]
+    results = [@root]
+    
+    while !stack.empty?
+      test = stack[-1]
+      puts results
+      return target if test.value == target
+      
+      if (!test.left_child.nil? && !results.include?(test.left_child))
+        stack << test.left_child
+        results << test.left_child
+      elsif (!test.right_child.nil? && !results.include?(test.right_child))
+        stack << test.right_child
+        results << test.right_child
+      else
+        stack.pop
+      end
+    end
+    return nil
+  end
 end
 
 a = Tree.new(10)
 a.build_tree([5,3,7,4])
+puts a.depth_first_search(7)
